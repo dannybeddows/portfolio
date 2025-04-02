716f1409037d7fbf5968e1271d7b701bc6204ddb
dannybeddows <danbeddows@gmail.com>
2025-04-02 12:41:36 +0700

    hello

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,10 +1,12 @@
-// main.js
-const currentPath = window.location.pathname;
-const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
-
-navLinks.forEach(link => {
-  if (link.getAttribute('href') === currentPath || 
-      (currentPath === '/' && link.getAttribute('href') === '/index.html')) {
-    link.classList.add('active');
-  }
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+  
+    navLinks.forEach(link => {
+      if (link.getAttribute('href') === currentPath || 
+          (currentPath === '/' && link.getAttribute('href') === '/index.html')) {
+        link.classList.add('active');
+      }
+    });
+  });
+  
