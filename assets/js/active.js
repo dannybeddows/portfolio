e1279deb32935c5226ed422e45565e07d7696e70
dannybeddows <danbeddows@gmail.com>
2025-04-02 12:52:31 +0700

    geh

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const currentPath = window.location.pathname;
-    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navLinks = document.querySelectorAll('.navbar-nav .nav .nav-link');
   
     navLinks.forEach(link => {
       if (link.getAttribute('href') === currentPath || 
